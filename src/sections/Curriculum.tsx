0b7bc20ab61bd538ca10fdbdf8a27408cda33bd5
Anthony Cyrille <ancyr@pm.me>
2023-07-12 08:22:05 +0200

    fix: typo

--- src/sections/Curriculum.tsx
+++ src/sections/Curriculum.tsx
@@ -63,7 +63,7 @@ export const Curriculum: React.FC<{}> = () => {
           </Paragraph>
         </Block>
         <Block>
-          <SubTitle>Pratique : application ReactJS</SubTitle>
+          <SubTitle>Pratique : application React</SubTitle>
           <Paragraph>
             Nous développerons ensemble{" "}
             <b>une application React en TypeScript avec NextJS en TDD</b> pour
@@ -74,7 +74,7 @@ export const Curriculum: React.FC<{}> = () => {
           </Paragraph>
         </Block>
         <Block>
-          <SubTitle>Pratique : application ReactJS</SubTitle>
+          <SubTitle>Pratique : application Nest</SubTitle>
           <Paragraph>
             Nous développerons ensemble{" "}
             <b>une application NestJS / TypeScript en TDD</b> reprenant les
